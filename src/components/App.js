3825b1b0c9c5b1b40d378d8a1872f67e6c7ff160
Atith794 <atith91098@gmail.com>
2024-01-25 22:53:18 +0530

    Assignment complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
 import React from "react";
 import './../styles/App.css';
+import {Route,Routes} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Navbar from "./Navbar";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
